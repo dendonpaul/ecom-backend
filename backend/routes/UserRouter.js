05a4253066b54f51930aad30b04b66dea6baec85
dendonpaul <dendonpaul@gmail.com>
2022-12-27 09:22:15 +0530

    userrouter updated

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const UserModel = require("../models/UserModel");
-const { createUser } = require("../controllers/UserController");
+const { createUser, editUser } = require("../controllers/UserController");
 
 router.post("/create", createUser);
+router.put("/edit", editUser);
+router.delete("/delete", deleteUser);
+router.get("/details", getDetails);
+router.get("/allUsers", getAllUsers);
 
 module.exports = router;
