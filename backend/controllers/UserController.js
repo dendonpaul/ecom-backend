2cd9d9db839f9d9e58950f5c7cd09f9ef12fdbde
dendonpaul <dendonpaul@gmail.com>
2022-12-28 22:08:45 +0530

    usercontroller and router updated

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const UserModel = require("../models/UserModel");
+const bcrypt = require("bcrypt");
 
 //signup
 const createUser = async (req, res) => {
@@ -20,4 +21,22 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+//login user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email });
+
+  //verify if user exists
+  if (!user) return res.status(401).json({ message: "Invalid Credentials" });
+
+  //check if passwords match
+  const passMatch = await bcrypt.compare(password, user.password);
+  if (!passMatch)
+    return res.status(401).json({ message: "Invalid credentials" });
+
+  //return user if credentials match
+  return res.status(200).json({ message: "Credentials Validated", user });
+};
+
+module.exports = { createUser, loginUser };
--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
 const UserModel = require("../models/UserModel");
-const { createUser, editUser } = require("../controllers/UserController");
+const {
+  createUser,
+  editUser,
+  deleteUser,
+  getDetails,
+  getAllUsers,
+  loginUser,
+} = require("../controllers/UserController");
 
 router.post("/create", createUser);
-router.put("/edit", editUser);
-router.delete("/delete", deleteUser);
-router.get("/details", getDetails);
-router.get("/allUsers", getAllUsers);
+// router.put("/edit", editUser);
+// router.delete("/delete", deleteUser);
+// router.get("/details", getDetails);
+// router.get("/allUsers", getAllUsers);
+router.post("/login", loginUser);
 
 module.exports = router;
